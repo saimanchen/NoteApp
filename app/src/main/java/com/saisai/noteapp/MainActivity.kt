5b110cdea70dbdb85acdac4a2d23ea98ff639334
saimanchen <saiman.chen@outlook.com>
2022-12-05 13:25:51 +0100

    add: RegisterScreen

--- app/src/main/java/com/saisai/noteapp/MainActivity.kt
+++ app/src/main/java/com/saisai/noteapp/MainActivity.kt
@@ -3,26 +3,14 @@ package com.saisai.noteapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.text.BasicTextField
-import androidx.compose.material.Divider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.*
-import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontStyle
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import com.saisai.noteapp.screens.LoginScreen
+import com.saisai.noteapp.screens.RegisterScreen
 import com.saisai.noteapp.ui.theme.NoteAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -35,23 +23,11 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    LoginScreen()
+                    RegisterScreen()
                 }
             }
         }
     }
-}
-
-@Composable
-fun Greeting() {
-    Column(modifier = Modifier
-        .padding(top = 192.dp)
-        .padding(start = 48.dp)
-    ) {
-
-    }
-
-
 }
 
 @Preview(showBackground = true)
